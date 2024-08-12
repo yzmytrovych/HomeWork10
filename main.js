d5e5a0935a3763086239b48dc5fcb6f204e467f1
Julia Zmytrovych <97659424+YuliyaZmytrovych@users.noreply.github.com>
2024-08-12 16:02:39 +0300

    first commit

--- main.js
+++ main.js
@@ -7,3 +7,5 @@ console.log('Hey, this is my JS program');
 console.log('Hey, this is my JS program'); 
 console.log('Hey, this is my JS program');
 >>>>>>> e4df021edb1e227cfc50da2f230d3a11cb7e350e
+
+12
